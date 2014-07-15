136d41f3e4f213e4419c1c881e9fc280a73c31a5
PaulPidou <pidou.paul@gmail.com>
2014-07-15 20:26:10 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,25 @@
 import tkinter as tk
+
+class Grid(Canvas):
+ "Main graphic class"
+ def __init__(self, rows=128, cols=128, cellSize=8):
+  Canvas.__init__(self)
+  self.rows, self.cols, self.cellSize = rows, cols, cellSize
+  self.configure(width = self.cellSize*self.rows, height = self.cellSize*self.cols)
+  self.drawGrid()
  
-window = tk.Tk()
-rows = 128
-cols = 128
-cellSize = 8
-canvas = tk.Canvas(window, width = cellSize*rows, height = cellSize*cols)
+ def drawGrid(self):
+  "Draw grid" 
+  for x in range(0, self.cellSize*self.rows, self.cellSize):
+   self.create_line(x, 0, x, self.cellSize * self.rows)
+ 
+  for y in range(0, self.cellSize*self.cols, self.cellSize):
+   self.create_line(0, y, self.cellSize * self.cols, y)
 
-##  Draw grid  
-for x in range(0, cellSize*rows, cellSize):
-    canvas.create_line(x, 0, x, cellSize * rows)
 
-for y in range(0, cellSize*cols, cellSize):
-    canvas.create_line(0, y, cellSize * cols, y)
-##
-
-canvas.pack()
-window.mainloop()
+# Programme de test
+if __name__ == '__main__':
+    window = tk.Tk()
+    gr = Grid(window)
+    gr.pack()
+    window.mainloop()
